6ab3647161fe6b8e3086b2162931a5f48710f4a6
alessia96 <66359835+alessia96@users.noreply.github.com>
2022-07-07 19:02:50 +0200

    Update reddit_analysis.py

--- reddit_analysis.py
+++ reddit_analysis.py
@@ -80,19 +80,7 @@ wordcloud.to_image()
 # LDA
 
 def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=1):
-    """
-    Compute c_v coherence for various number of topics
-    Parameters:
-    ----------
-    dictionary : Gensim dictionary
-    corpus : Gensim corpus
-    texts : List of input texts
-    limit : Max num of topics
-    Returns:
-    -------
-    model_list : List of LDA topic models
-    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
-    """
+    # Compute c_v coherence for various number of topics
     coherence_values = []
     model_list = []
     for num_topics in range(start, limit, step):
